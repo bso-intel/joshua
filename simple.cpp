c69abb3cf7f6258b08bf85f8756638eabbab146c
Byoungro So <byoungro.so@intel.com>
2020-09-06 12:18:17 -0700

    Clang formatted

--- simple.cpp
+++ simple.cpp
@@ -1,8 +1,7 @@
-  #include <iostream>
-int  main(int argc,   char* argv[ ] )
-{
+#include <iostream>
+int main(int argc, char *argv[]) {
   int i = 3;
-    int* ptr = & i ;
-  std::cout<<i<< ptr<<std::endl;
-     return 0;
+  int *ptr = &i;
+  std::cout << i << ptr << std::endl;
+  return 0;
 }
